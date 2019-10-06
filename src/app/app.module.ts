fe5d6afb16d6cd75f81bd4dd86c0c289bf717969
Guillermo <guicao3@hotmail.com>
2019-10-06 20:32:53 -0300

    added firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +21,9 @@ import { ViewWrapperComponent } from './view-wrapper/view-wrapper.component';
    imports: [
       BrowserModule,
       AppRoutingModule,
-      NgbModule
+      NgbModule,
+      AngularFireModule.initializeApp(environment.firebase),
+      AngularFirestoreModule
    ],
    providers: [],
    bootstrap: [
