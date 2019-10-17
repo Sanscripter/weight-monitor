5d0529c4fec7f6d4e5599189fff08c5fd38c11b0
Guillermo <guicao3@hotmail.com>
2019-10-16 22:31:44 -0300

    updated wrapper and home  template and styles to prevent data 'overflowing' styled portion of content

--- src/app/view-wrapper/view-wrapper.component.ts
+++ src/app/view-wrapper/view-wrapper.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-wrapper',
   templateUrl: './view-wrapper.component.html',
-  styleUrls: ['./view-wrapper.component.css']
+  styleUrls: ['./view-wrapper.component.scss']
 })
 export class ViewWrapperComponent implements OnInit {
 
