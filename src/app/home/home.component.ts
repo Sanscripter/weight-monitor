6d7c9f2d62e920306f207143f1bf081b4c3097bf
Sanscripter <guicao3@hotmail.com>
2019-10-17 22:55:43 -0300

    WIP home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { WeightModel } from '../models/weight-model';
 import { WeightsService } from '../services/firebase/weights.service';
@@ -6,18 +6,45 @@ import { WeightsService } from '../services/firebase/weights.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   public weights: WeightModel[];
+  private user = {
+    email: 'otheruser@example.com'
+  };
 
   constructor(private weightsService: WeightsService) { }
 
   ngOnInit() {
-    this.weightsService.getWeightList().subscribe(data => {
-      console.log(data);
-      this.weights = data;
+    this.weightsService.getListUserList(this.user).subscribe(data => {
+      this.weights = data.sort((a, b) => {
+        return a.date > b.date ? 1 : -1;
+      });
+
+
     });
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.weights);
+
+    const newWeight = {
+      value: Math.random() * 100 + 70,
+      user: this.user.email,
+      date: new Date(),
+      active: true
+    };
+
+    this.weightsService.add(newWeight);
+  }
+
+  public deleteWeight(id: string) {
+    this.weightsService.delete(id);
+  }
+
+  public updateWeight(weight: WeightModel) {
+    this.weightsService.update(weight);
   }
 
 }
