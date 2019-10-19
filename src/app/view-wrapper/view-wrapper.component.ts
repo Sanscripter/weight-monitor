cd920ef894c16695eab1087ab31b853fdb364459
Sanscripter <guillermoasmurua@gmail.com>
2019-10-19 20:05:42 -0300

    removed test logic from wrapper

--- src/app/view-wrapper/view-wrapper.component.ts
+++ src/app/view-wrapper/view-wrapper.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-view-wrapper',
   templateUrl: './view-wrapper.component.html',
@@ -8,10 +7,6 @@ import { Component, OnInit } from '@angular/core';
 export class ViewWrapperComponent implements OnInit {
 
   constructor() { }
-
-
-  private menino: string;
-
 
   ngOnInit() {
 
