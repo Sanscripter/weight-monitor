94a9ea64573809773f8911b731da7594110368a2
Sanscripter <guillermoasmurua@gmail.com>
2019-10-20 19:02:44 -0200

    Implemented chart component through chartjs

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label} from 'ng2-charts';
+import { WeightsService } from '../services/firebase/weights.service';
+import { WeightModel } from '../models/weight-model';
+import * as moment from 'moment';
+import { HelperService } from '../services/_helper.service';
+
+
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +15,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private weightService: WeightsService,
+              private helperService: HelperService) { }
+
+  private user = {
+    email: 'otheruser@example.com'
+  };
+  private weightList: WeightModel[];
+  private chartWeightData: ChartDataSets[] = [{ data: [] }];
+  public chartWeightLabels: Label[] = [];
+
+  public chartWeightOptions: (ChartOptions) = {
+    responsive: true,
+    maintainAspectRatio: false,
+    elements: {
+      line: {
+        tension: 0.1,
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          type: 'time',
+          time: {
+            unit: 'day'
+          }
+        }
+      ]
+    }
+  };
+
+  public chartWeightColors: Color[] = [
+    {
+      borderColor: 'green',
+      backgroundColor: 'rgba(0,255,0,0.3)',
+    },
+  ];
+  public chartWeightLegend = false;
+  public chartWeightType = 'line';
+  public chartWeightPlugins = [];
 
   ngOnInit() {
+
+    this.weightService.getUserWeights(this.user).subscribe((data) => {
+      this.weightList = data;
+      this.updateChart();
+    });
   }
 
+  private updateChart() {
+    this.chartWeightData[0].data = [];
+    this.chartWeightLabels = [];
+    this.weightList = this.weightList.sort((a, b) => this.helperService.sortByDate(a, b));
+    this.weightList.forEach(weightData => {
+      const formatDate = moment(weightData.date);
+      this.chartWeightData[0].data.push(weightData.value);
+      this.chartWeightLabels.push(formatDate);
+    });
+  }
 }
